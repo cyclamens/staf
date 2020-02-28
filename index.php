173388516663e84caae129bd403232947161dd2c
staf <fatfa83@yahoo.fr>
2020-02-28 06:26:46 +0100

    utilisation d'architecture MVC

--- index.php
+++ index.php
@@ -1,41 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mon blog</title>
-	<link href="style.css" rel="stylesheet" /> 
-    </head>
-        
-    <body>
-        <h1>Mon super blog !</h1>
-        <p>Derniers billets du blog :</p>
- 
 <?php
-// Connexion à la base de données
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
+require('model.php');
 
-// On récupère les 5 derniers billets
-$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+$posts = getPosts();
 
-while ($donnees = $req->fetch())
-{
-?>
-<div class="news">
-   <?php include("affiche_billet.php"); ?>
-    <br />
-    <em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
-    </p>
-</div>
-<?php
-} // Fin de la boucle des billets
-$req->closeCursor();
-?>
-</body>
-</html>
+require('indexView.php');
