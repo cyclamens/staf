2e414ac10c1bbbfd0bd78192ad40918682922d0c
staf <fatfa83@yahoo.fr>
2020-04-24 14:23:14 +0200

    initialisation mot de pass base de données

--- model/Manager.php
+++ model/Manager.php
@@ -5,7 +5,7 @@ class Manager
 {
     protected function dbConnect()
     {
-        $db = new \PDO('mysql:host=localhost;dbname=id13360865_blog_ecrivain;charset=utf8', 'id13360865_taf', 'Diamboulou-83');
+        $db = new \PDO('mysql:host=localhost;dbname=blog_ecrivain;charset=utf8', 'root', '');
         return $db;
     }
 }
